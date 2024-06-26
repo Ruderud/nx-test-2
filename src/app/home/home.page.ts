6d7c6c74c9e74301dd62b6f3bd1778c89771f8aa
ruderud <jungkh05@naver.com>
2024-06-26 09:10:26 +0900

    dev-home rechage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,58 +11,58 @@ type Todo = {
   completed: boolean;
 };
 
-@Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-})
-export class HomePage implements OnInit {
-  todos: Todo[] = [];
-  constructor(private router: Router) {}
-
-  openNewTodo(todoId: number) {
-    this.router.navigate(['/todos', todoId]);
-  }
-
-  ngOnInit() {
-    this.todos = todos;
-    this.generateItems();
-  }
-
-  items: string[] = [];
-
-  private generateItems() {
-    const count = this.items.length + 1;
-    for (let i = 0; i < 50; i++) {
-      this.items.push(`Item ${count + i}`);
-    }
-  }
-
-  onIonInfinite(ev: InfiniteScrollCustomEvent, t: string) {
-    console.log('ev', ev, t);
-    // this.generateItems();
-    // setTimeout(() => {
-    //   (ev as InfiniteScrollCustomEvent).target.complete();
-    // }, 500);
-  }
-}
-
 // @Component({
 //   selector: 'app-home',
-//   template: `
-//     <div>
-//       <cdk-virtual-scroll-viewport itemSize="50" class="example-viewport">
-//         <div
-//           *cdkVirtualFor="let item of items; let i = index"
-//           class="example-item"
-//         >
-//           {{ item }}
-//         </div>
-//       </cdk-virtual-scroll-viewport>
-//     </div>
-//   `,
+//   templateUrl: 'home.page.html',
 //   styleUrls: ['home.page.scss'],
 // })
-// export class HomePage {
-//   items = Array.from({ length: 1000 }, (_, i) => `Item ${i + 1}`);
+// export class HomePage implements OnInit {
+//   todos: Todo[] = [];
+//   constructor(private router: Router) {}
+
+//   openNewTodo(todoId: number) {
+//     this.router.navigate(['/todos', todoId]);
+//   }
+
+//   ngOnInit() {
+//     this.todos = todos;
+//     this.generateItems();
+//   }
+
+//   items: string[] = [];
+
+//   private generateItems() {
+//     const count = this.items.length + 1;
+//     for (let i = 0; i < 50; i++) {
+//       this.items.push(`Item ${count + i}`);
+//     }
+//   }
+
+//   onIonInfinite(ev: InfiniteScrollCustomEvent, t: string) {
+//     console.log('ev', ev, t);
+//     // this.generateItems();
+//     // setTimeout(() => {
+//     //   (ev as InfiniteScrollCustomEvent).target.complete();
+//     // }, 500);
+//   }
 // }
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <div>
+      <cdk-virtual-scroll-viewport itemSize="50" class="example-viewport">
+        <div
+          *cdkVirtualFor="let item of items; let i = index"
+          class="example-item"
+        >
+          {{ item }}
+        </div>
+      </cdk-virtual-scroll-viewport>
+    </div>
+  `,
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  items = Array.from({ length: 1000 }, (_, i) => `Item ${i + 1}`);
+}
